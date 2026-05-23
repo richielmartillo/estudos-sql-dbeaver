8933f14db9d7e9b3664ce36cacf25ac447f7ee9f
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-05-23 19:12:21 -0300

    Adiciona consultas SQL do curso

--- consultas.sql
+++ consultas.sql
@@ -22,3 +22,31 @@ SELECT *
 FROM Track
 ORDER BY Composer ASC
 LIMIT 10;
+
+-- Selecionar clienetes de uma cidade específica
+SELECT * 
+FROM Customer
+WHERE City = 'São Paulo';
+
+-- Selecionar clientes de um país específico
+SELECT FirstName, LastName, Country
+FROM Customer 
+WHERE Country = 'Brazil';
+
+SELECT * 
+FROM Invoice
+WHERE Total < 1.98
+
+-- Selecionar faturas com filtro de país e estado
+SELECT *
+FROM Invoice 
+WHERE BillingCountry = 'Brazil'
+AND BillingState = 'SP'
+AND Total >= 5.00
+
+-- Selecionar faturas do estado de SP ou do RJ
+SELECT *
+FROM Invoice
+WHERE BillingState = 'SP'
+OR BillingState = 'RG'
+ORDER BY BillingState ASC
